c3ff08546496e112fc94d5ed5c8337ded8f16e58
chenyinkai <287696322@qq.com>
2018-05-17 20:04:21 +0800

    增加计数器

--- src/components/common/footerBar.vue
+++ src/components/common/footerBar.vue
@@ -8,6 +8,15 @@
         <font-awesome-icon :icon="['fas', 'heart']" />
         <span>善良的乌贼</span>
       </div>
+      <div class="footer-inner">
+        <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
+        |
+        <span id="busuanzi_container_site_uv">
+          您是第<span id="busuanzi_value_site_uv"></span>个来访者
+        </span>
+        |
+        <a href="http://www.miitbeian.gov.cn/">浙ICP备18020724号-1</a>
+      </div>
     </footer>
   </div>
 </template>
@@ -46,7 +55,7 @@ footer {
   color: #999;
   .footer-inner {
     text-align: center;
-    margin: 20px 0;
+    margin: 5px 0;
   }
 }
 </style>
