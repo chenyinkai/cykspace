9b0673e5ba7488d0cef720d20638255ca6088cf3
chenyinkai <287696322@qq.com>
2018-05-27 18:48:46 +0800

    增加注释

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,9 @@
+/**
+ * 时间戳转时间格式
+ *
+ * @param  {date, type} 时间戳, 格式类型
+ * @return {date} 返回 yyyy-mm-dd 或者 mm-dd
+ */
 export const formatTime = (date, type) => {
   if (type === 'yyyy-mm-dd') {
     return `${new Date(date).getFullYear()}-${(new Date(date).getMonth() + 1) < 10 ? ('0' + (new Date(date).getMonth() + 1)) : (new Date(date).getMonth() + 1)}-${(new Date(date).getDate()) < 10 ? ('0' + new Date(date).getDate()) : new Date(date).getDate()}`
