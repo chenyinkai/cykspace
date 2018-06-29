947530f034e64267c4d9dc9f34b278846342bfa2
chenyinkai <287696322@qq.com>
2018-06-29 17:16:15 +0800

    解决计数器只在首页显示bug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="footer-inner">
+      <span id="busuanzi_container_site_pv">本站总访问量
+        <span id="busuanzi_value_site_pv"></span>次</span>
+      |
+      <span id="busuanzi_container_site_uv">
+        您是第
+        <span id="busuanzi_value_site_uv"></span>个来访者
+      </span>
+      |
+      <a href="http://www.miitbeian.gov.cn/">浙ICP备18020724号-1</a>
+    </div>
   </div>
 </template>
 
@@ -10,6 +21,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.footer-inner {
+  font-size: 14px;
+  color: #999;
+  text-align: center;
+  margin: 5px 0;
+}
 </style>
--- src/components/common/footerBar.vue
+++ src/components/common/footerBar.vue
@@ -8,15 +8,6 @@
         <font-awesome-icon :icon="['fas', 'heart']" />
         <span>善良的乌贼</span>
       </div>
-      <div class="footer-inner">
-        <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
-        |
-        <span id="busuanzi_container_site_uv">
-          您是第<span id="busuanzi_value_site_uv"></span>个来访者
-        </span>
-        |
-        <a href="http://www.miitbeian.gov.cn/">浙ICP备18020724号-1</a>
-      </div>
     </footer>
   </div>
 </template>
