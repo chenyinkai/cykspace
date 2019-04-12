c9dc069fea59c376e4ee9c39fbb8f4b74526f3de
chenyinkai <287696322@qq.com>
2019-04-12 15:18:28 +0800

    fix: 移除计数器

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,6 @@
     <footer-bar></footer-bar>
     <scroll-bar></scroll-bar>
     <div class="footer-inner">
-      <span id="busuanzi_container_site_pv">本站总访问量
-        <span id="busuanzi_value_site_pv"></span>次</span>
-      |
-      <span id="busuanzi_container_site_uv">
-        您是第
-        <span id="busuanzi_value_site_uv"></span>个来访者
-      </span>
-      |
       <a href="http://www.miitbeian.gov.cn/">浙ICP备18020724号-1</a>
     </div>
     <qrcode></qrcode>
